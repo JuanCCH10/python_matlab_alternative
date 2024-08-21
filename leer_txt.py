9bf38e8483f5068bc7eeebda1f98b2ee927e41b0
Juan Carlos Chavez Hermoso <jcchavezh@unindetec.edu.mx>
2024-08-21 11:55:44 -0600

    se solucionó la creación de archivos con nombres numerados, sigue pendiete el cambio de formato del nombre y la información en los archivos

--- leer_txt.py
+++ leer_txt.py
@@ -17,21 +17,8 @@ archivo = "simbolos.txt"
 # llamado de función 
 array1 = leer_txt(archivo)  #no es posible imprimir la variable de manera directa, deben usarce readline() o readlines()
 
-# tipo de variable
-print(type(array1))
-
-# cambio de tipo mediante contructores
-#array_str = str(array1)
-#print(type(array_str))      #esta conversión es erronea
-#los contructores int / float no aceptan tipos que no sean: str, byte o real_num
-
 # conversión a lista
 lista = array1.readlines()
-
-# mostrar el arreglo en este punto
-for i in range(0,3):
-    print(lista[i])
-#es posible asignar uno a uno los valores
 
 # calculo de indices
 #indices de nid n/a:     0->31
@@ -40,27 +27,12 @@ for i in range(0,3):
 #indices de b2 3/4:      B+1->(B+98) = C
 #indices de b3 3/4:      C+1->(C+98)
 #el archivo llega hasta la línea 424, es decir el indice 423 por inicar desde 0
-#primer intento con indices a mano
 nid_long = 32
 block_long = 98
-nid_block = [lista[i] for i in range(0,nid_long)]
-#longitud de la lista resultante
-print(len(nid_block))
-print(nid_block)
-block1 = [lista[i] for i in range(nid_long, nid_long+block_long)]
-print(len(block1))
-print(block1)
-block2 = [lista[i] for i in range(nid_long+block_long, nid_long+block_long*2)]
-print(block2)
-print(len(block2))
-block3 = [lista[i] for i in range(nid_long+block_long*2, nid_long+block_long*3)]
-
-
-# simplificar el código 
 block_num = 3 # sin considerar el nid
 for i_aux in range(0,block_num):
-        block = [lista[i] for i in range(nid_long+block_long*i,nid_long+block_long*(i+1))]
-        nombre = ["block_"+ str(i+1)]
+        block = [lista[i] for i in range(nid_long+block_long*i_aux,nid_long+block_long*(i_aux+1))]
+        nombre = ["block_"+ str(i_aux+1)]
         nombre = str(nombre)
         block = str(block)
         escribir_txt(nombre,block)
