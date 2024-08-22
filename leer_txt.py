7fb7e859f45ce7c4bfb14457e9c232a5ea51d07f
Juan Carlos Chavez Hermoso <jcchavezh@unindetec.edu.mx>
2024-08-22 15:49:13 -0600

    Pendiente el formato de los datos desde la lectura

--- leer_txt.py
+++ leer_txt.py
@@ -1,22 +1,28 @@
 from tools import *
 
-print("inicio")
-
 # nombre del archivo
 archivo = "simbolos.txt"
 
 # llamado de función 
-array1 = leer_txt(archivo)  #no es posible imprimir la variable de manera directa, deben usarce readline() o readlines()
+array1 = leer_txt(archivo)
 
 # conversión a lista
-lista = array1.readlines()
+#lista = np.array(array1.readlines())
+lista = array1
+print(type(lista))
 
-#el archivo llega hasta la línea 424, es decir el indice 423 por inicar desde 0
+# https://astrofrog.github.io/py4sci/_static/08.%20Reading%20and%20writing%20files.html
+# revisar como va tratando la información leída hasta llegar a la extracción de valores numericos
+
+# 
 nid_long = 32
 block_long = 98
 block_num = 3 # sin considerar el nid
 for i_aux in range(0,block_num):
         block = [lista[i] for i in range(nid_long+block_long*i_aux,nid_long+block_long*(i_aux+1))]
+        block = np.array(block)
+        print(type(block))
+        print(block)
         nombre = 'block_'+ str(i_aux+1)
         
 
@@ -24,11 +30,9 @@ for i_aux in range(0,block_num):
         #escribir_txt(nombre,block)
 
 
-count = np.arange(98)
-print(count)
-count = interleave(count)
-print(count)
-count = d_interleave(count)
-print(count)
-
-print("fin")
+#count = np.arange(98)
+#print(count)
+#count = interleave(count)
+#print(count)
+#count = d_interleave(count)
+#print(count)
