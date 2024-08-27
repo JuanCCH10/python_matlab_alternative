9b77a7d992fc6f43e9f99788ac012cfee44b2419
Juan Carlos Chavez Hermoso <jcchavezh@unindetec.edu.mx>
2024-08-27 10:46:38 -0600

    Se tiene el programa para segmentar la información en bloques, desentrelazar y generar los archivos para simulación desde Vivado

--- leer_txt.py
+++ leer_txt.py
@@ -1,38 +1,21 @@
 from tools import *
 
-# nombre del archivo
-archivo = "simbolos.txt"
+# nombre del archivo fuente
+archivo = "Datos de entrada FTP/03_MSN_D_Confirm.txt"
+# nomenclatura de los archivos resultado
+file_gen_name = '03_block_'
+block_num = 5 # sin considerar el nid
 
-# llamado de función 
 array1 = leer_txt(archivo)
 
-# conversión a lista
-#lista = np.array(array1.readlines())
-lista = array1
-print(type(lista))
+lista_num = [str(num.strip()) for num in array1] # asignar el contenido del archivo a una lista
 
-# https://astrofrog.github.io/py4sci/_static/08.%20Reading%20and%20writing%20files.html
-# revisar como va tratando la información leída hasta llegar a la extracción de valores numericos
-
-# 
 nid_long = 32
 block_long = 98
-block_num = 3 # sin considerar el nid
+
 for i_aux in range(0,block_num):
-        block = [lista[i] for i in range(nid_long+block_long*i_aux,nid_long+block_long*(i_aux+1))]
-        block = np.array(block)
-        print(type(block))
-        print(block)
-        nombre = 'block_'+ str(i_aux+1)
-        
-
-        #block = str(block)
-        #escribir_txt(nombre,block)
-
-
-#count = np.arange(98)
-#print(count)
-#count = interleave(count)
-#print(count)
-#count = d_interleave(count)
-#print(count)
+        block = [lista_num[i] for i in range(nid_long+block_long*i_aux,nid_long+block_long*(i_aux+1))]   #recorte de bloques
+        nombre = file_gen_name + str(i_aux+1)                                                            #nombre del archivo
+        block = d_interleave(block)
+        for i in range(0,block_long):
+                escribir_txt(nombre,block[i]+'\n')
--- tools.py
+++ tools.py
@@ -7,16 +7,16 @@ def leer_txt(file_name):
 
 # función para escritura de archivos
 def escribir_txt(file_name,content):
-    escritura = open(file_name,"w")
+    escritura = open(file_name,"a")
     escritura.write(content)
     escritura.close()
 
 # función para desentrelazar simbolos
 def d_interleave(lista):
-    recover = np.zeros(98)
+    recover = []
     index = [0,1,8,9,16,17,24,25,32,33,40,41,48,49,56,57,64,65,72,73,80,81,88,89,96,97,2,3,10,11,18,19,26,27,34,35,42,43,50,51,58,59,66,67,74,75,82,83,90,91,4,5,12,13,20,21,28,29,36,37,44,45,52,53,60,61,68,69,76,77,84,85,92,93,6,7,14,15,22,23,30,31,38,39,46,47,54,55,62,63,70,71,78,79,86,87,94,95] 
-    for i in range(98):
-        recover[i] = lista[index[i]]
+    for i in range(len(index)):
+        recover.append(lista[index[i]])
     return recover
 
 # función para entrelazar datos en una lista
