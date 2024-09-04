f0cbb06b3ff6d4e4f3bb566e1e76aca4a46994f7
Juan Carlos Chavez Hermoso <jcchavezh@unindetec.edu.mx>
2024-09-04 10:24:26 -0600

    Archivo para comparación de datos  origen-recuperación de módulos trellis-viterbi locales

---
+++ compare_txt.py
@@ -0,0 +1,29 @@
+from tools import *
+
+file1 = 'in_dib0.txt'
+file2 = 'recover_dib0.txt'
+
+info1 = leer_txt(file1)
+info2 = leer_txt(file2)
+
+list1 = [str(num.strip()) for num in info1]
+list2 = [str(num.strip()) for num in info2]
+
+list2.remove('UU')  # se remueve el primer dato que es un encabezado de escritura
+
+aciertos = 0
+errores = 0
+indices_error = []
+for i in range(len(list1)):
+    if list1[i] == list2[i]:
+        aciertos = aciertos + 1
+    else:
+        errores = errores + 1
+        indices_error.append(i)
+
+print('# de datos comparados',len(list1))
+if errores == 0:
+    print('________________DATOS IGUALES________________')
+else:
+    print('# de errores: ', errores)
+    print('indices de error: ',indices_error)
